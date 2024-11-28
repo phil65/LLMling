6044789a49888d32cd33905603762d338fbad3e7
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-11-28 16:02:39 +0100

    Bump version 0.6.2 → 0.6.3

--- src/llmling/__init__.py
+++ src/llmling/__init__.py
@@ -17,7 +17,7 @@ from llmling.processors.registry import ProcessorRegistry
 from llmling.server import LLMLingServer, serve
 
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 __all__ = [
     "ConfigError",
