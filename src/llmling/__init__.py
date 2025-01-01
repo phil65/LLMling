f67983eb51c66ca2bab25366d743714ff576fb43
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-01-01 23:50:23 +0100

    Bump version 1.5.6 → 1.6.0

--- src/llmling/__init__.py
+++ src/llmling/__init__.py
@@ -32,7 +32,7 @@ from llmling.config.models import (
 from llmling.config.store import ConfigStore
 from llmling.core.baseregistry import BaseRegistry
 
-__version__ = "1.5.6"
+__version__ = "1.6.0"
 
 __all__ = [
     "BasePrompt",
